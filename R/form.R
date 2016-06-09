a9122dd9a3c74a61853a367c5f3b2d2967f5e554
Enrico Schumann <es@enricoschumann.net>
2016-06-09 18:05:15 +0200

    DESCRIPTION (Depends): add 'R (>= 3.2)' because 'form' uses 'lengths'
    form: fixed -- the function now works when
    'useFancyQuotes' is not FALSE (before, the function
    failed or produced warnings)
    
    form: new argument 'form.options' for passing options

--- R/form.R
+++ R/form.R
@@ -1,4 +1,7 @@
-form <- function(..., cat = TRUE, perl = "perl") {
+form <- function(..., cat = TRUE, perl = "perl", form.options) {
+    oo <- getOption("useFancyQuotes")
+    options(useFancyQuotes = FALSE)
+    on.exit(options(useFancyQuotes = oo))
 
     s <- list(...)
     vecs <- lengths(s) > 1L
@@ -9,7 +12,15 @@ form <- function(..., cat = TRUE, perl = "perl") {
     }
     s <- unlist(s)
     cmd <- paste(dQuote(s), collapse = ",", sep = "")
-    cmd <- paste0("use Perl6::Form; print form ", cmd, ";")
+
+    opts <- if (!missing(form.options)) {
+                paste0("{",
+                       paste(paste(names(form.options), "=>", dQuote(form.options)),
+                             collapse = ", "),
+                       "}, ")
+            } else ""
+                    
+    cmd <- paste0("use Perl6::Form; print form ", opts, " ", cmd, ";")
     res <- system2(perl, args = paste("-e ", shQuote(cmd)), stdout = TRUE)
     if (cat) {
         cat(paste(res, collapse ="\n"), "\n")
